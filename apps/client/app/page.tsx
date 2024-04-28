d7ed7e7b0b6968fef7a66f634fe61b0efa17e9e2
Sayan67 <sayan.das.21@aot.edu.in>
2024-04-28 23:19:38 +0530

    deleting previous structure

--- apps/client/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Card } from "@repo/ui/card";
-
-export default function Page(): JSX.Element {
-  return (
-    <>
-      <Card />
-    </>
-  );
-}
--- packages/ui/src/card.tsx
+++
@@ -1,7 +0,0 @@
-export function Card(): JSX.Element {
-  return (
-    <h2 className="text-red-500">
-      Hello World
-    </h2>
-  );
-}
