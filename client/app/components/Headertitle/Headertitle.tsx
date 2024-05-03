ae4239749a562c042fd9b0c815286ef81cddd548
Indrani Som <indranisom39@gmail.com>
2024-05-03 17:35:54 +0530

    added the headertitle

---
+++ client/app/components/Headertitle/Headertitle.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+const Headertitle = () => {
+  return (
+    <div className="w-full flex pt-7 bg-white h-56 gap-1 pl-20 items-center">
+      <h1 className="text-[4rem] font-anton text-primary_text leading-[0.9] tracking-wider">
+        ABOUT
+      </h1>
+      <h1 className="text-[3rem] font-poppins font-semibold text-primary_text leading-[0.9] tracking-wider transform -rotate-90 ">
+        US
+      </h1>
+      <h4 className="text-[1rem] font-montserrat text-primary_text leading-[0.9] pt-10 tracking-wider">
+        We are a group of students from (SCCSE){" "}
+      </h4>
+    </div>
+  );
+};
+export default Headertitle;
