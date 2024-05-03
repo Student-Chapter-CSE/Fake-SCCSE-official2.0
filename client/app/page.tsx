375528e437ed1a9dfc84daecf241bc12568ec7e0
Sayan67 <sayan.das.21@aot.edu.in>
2024-05-03 20:22:12 +0530

    Fixed the hero section

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/app/components/hero/Hero";
 export default function Home() {
+
   return (
     <main className="min-h-screen bg-background flex gap-4 text-primary_text items-end">
       <Hero/>
