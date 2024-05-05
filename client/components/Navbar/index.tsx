6cce5a557aaf476aef45fc1413547b423e7d0af3
ABHIRUP <abhirupsaha02@gmail.com>
2024-05-05 23:02:58 +0530

    navbar some fix

--- client/components/Navbar/index.tsx
+++ client/components/Navbar/index.tsx
@@ -1,76 +1,66 @@
 import React from "react";
 import SClogosvg from "/public/sclogo.svg";
-import Home from "/public/icons/Home_light.svg";
-import Team from "/public/icons/teams.svg";
-import Event from "/public/icons/event.svg";
 import Contact from "/public/icons/mail.svg";
+import Event from "/public/icons/event.svg";
+import Home from "/public/icons/Home_light.svg";
 import Join from "/public/icons/joinus.svg";
+import Team from "/public/icons/teams.svg";
 import Link from "next/link";
+
+const navItemTextStyle = "transform text-xs -rotate-90"
+const navItemBorderStyle = "relative border border-gray-600 h-14 p-6 rounded"
+const logoPositionStyle = "absolute bottom-0 right-0"
 
 const Navbar: React.FC = () => {
   return (
-    <div className="h-[72px] w-full border-b-[0.5px] border-primary_text fixed bg-background z-10 py-2 ">
-      <div className="hidden md:flex justify-between items-center  pr-12">
+    <div className="h-[72px] w-full border-b-[0.5px] border-primary_text fixed bg-background z-10 py-2 px-4">
+      <div className="hidden md:flex justify-between">
         {/*need to add the hamburger here*/}
-
         <SClogosvg />
 
-        <div className="flex items-center space-x-10">
+        <div className="flex">
           <Link href="#hero">
-            <div className="flex relative space-x-1">
-              <div className="border border-gray-600 space-x-1 h-14 p-6 rounded relative">
-                <div className="border-y-2 border-gray-600 " />
-
-                <Home className="absolute bottom-0 right-0" />
-
-                <p className="absolute bottom-1 pb-3 left-10 transform -rotate-90 text-xs  ">
-                  Home
-                </p>
+            <div className="flex">
+              <div className={navItemBorderStyle}>
+                <Home className={logoPositionStyle} />
               </div>
+              <span className={navItemTextStyle}>Home</span>
             </div>
           </Link>
+
           <Link href="#about">
-            <div className="flex relative space-x-1">
-              <div className="border border-gray-600 space-x-1 h-14 p-6 rounded relative">
-                <Team className="absolute bottom-0 right-0" />
-
-                <p className="absolute bottom-1 pb-3 left-10 transform -rotate-90 text-xs  ">
-                  Aboutus
-                </p>
+            <div className="flex">
+              <div className={navItemBorderStyle}>
+                <Team className={logoPositionStyle} />
               </div>
+              <span className={navItemTextStyle}>About Us</span>
             </div>
           </Link>
+
           <Link href="#events">
-            <div className="flex relative space-x-1">
-              <div className="border border-gray-600 space-x-1 h-14 p-6 rounded relative">
-                <Event className="absolute bottom-0 right-0" />
-
-                <p className="absolute bottom-1 pb-3 left-10 transform -rotate-90 text-xs  ">
-                  Events
-                </p>
+            <div className="flex">
+              <div className={navItemBorderStyle}>
+                <Event className={`${logoPositionStyle} right-0.5`} />
               </div>
+              <span className={navItemTextStyle}>Events</span>
             </div>
           </Link>
+
           <Link href="#contact">
-            <div className="flex relative space-x-1">
-              <div className="border border-gray-600  h-14 p-6 rounded relative">
-                <Contact className="absolute bottom-0 right-0" />
-
-                <p className="absolute bottom-2 pb-4 left-10 transform -rotate-90 text-[0.6rem]  ">
-                  ContactUs
-                </p>
+            <div className="flex">
+              <div className={navItemBorderStyle}>
+                <Contact className={`${logoPositionStyle} right-0.5`} />
               </div>
+              <span className={navItemTextStyle}>Contact Us</span>
             </div>
           </Link>
-          <Link href="#">
-            <div className="flex relative space-x-1">
-              <div className="border border-gray-600 space-x-1 h-14 p-6 rounded relative">
-                <Join className="absolute bottom-0 right-0" />
 
-                <p className="absolute bottom-2 pb-3 left-10 transform -rotate-90 text-xs  ">
-                  JoinUs
-                </p>
+          <Link href="#">
+            <div className="flex">
+              <div className={navItemBorderStyle}>
+                <Join className={`${logoPositionStyle} bottom-0.5 right-1`} />
               </div>
+              <span className={navItemTextStyle}>Join Us</span>
             </div>
           </Link>
         </div>
