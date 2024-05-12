4a9df8489684804fec7ec64ee57f5a4ab5f4851c
Ashish Gupta <gupta.ashish2694@gmail.com>
2024-05-13 00:55:19 +0530

    bug:minor-fixes-v4

---
+++ client/app/events/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import EventPages from '@/components/EventPages'
+const page = () => {
+  return (
+    <div>
+      <EventPages />
+    </div>
+  )
+}
+
+export default page
